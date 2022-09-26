bd2da471c3a897e3cc80de1f772b9d9d0995e514
invek <vivekkumar.oct17@gmail.com>
2022-09-26 20:36:50 +0530

    New Type of Array

---
+++ STL/std_array.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    // Declaring an std::array
+    std::array <int, 5> arr;
+
+    std::array <int,3> myarray = {10,20,30};
+
+    std::array <int,3> myarray2;              //can use like this also
+    myarray = {9,8,7};
+
+
+return 0;
+}
