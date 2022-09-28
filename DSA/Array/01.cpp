36f54320606a5af09e7b63c1c2a727c1af0811c5
invek <vivekkumar.oct17@gmail.com>
2022-09-28 14:50:27 +0530

    Making PrintArray funct

--- DSA/Array/01.cpp
+++ DSA/Array/01.cpp
@@ -1,5 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
+void printArray(int arr[],int size){
+    cout<<"Printing the Array"<<endl;
+
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+}
 int main(){
     
     // // std::array<int,100> arr; 
@@ -8,10 +15,13 @@ int main(){
     // // fill_n(arr,100,-69);
     // cout<<"Value is  : " <<arr[59];
 
-    int arr[5] ={ 10,20,30,40,50};
+    // int arr[5] ={ 10,20,30,40,50};
 
-    for(int i=0;i<5;i++){
-        cout<<arr[i]<<" ";
-    }
+    // for(int i=0;i<5;i++){
+    //     cout<<arr[i]<<" ";
+    // }
+
+    int arr[6] = {-60};
+    printArray(arr,6);
 return 0;
 }
