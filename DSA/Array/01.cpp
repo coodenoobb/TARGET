e6cd01b4c5812f4fd810e8f14408f940fd6b0e8a
invek <vivekkumar.oct17@gmail.com>
2022-09-28 14:45:01 +0530

    Printing the Array

---
+++ DSA/Array/01.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    // // std::array<int,100> arr; 
+    // // arr.fill(-20);
+
+    // // fill_n(arr,100,-69);
+    // cout<<"Value is  : " <<arr[59];
+
+    int arr[5] ={ 10,20,30,40,50};
+
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
+return 0;
+}
