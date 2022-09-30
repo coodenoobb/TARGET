30f987c052d31c824e5dc9d12670619e49953916
invek <vivekkumar.oct17@gmail.com>
2022-09-30 20:16:25 +0530

    Binary Search

---
+++ DSA/Array/09_Binary_search.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printArray(int arr[], int size)
+{
+    cout << "Printing the Array" << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int binarySearch(int arr[],int size, int key){
+    int l=0;
+    int h=size-1;
+    int mid = (l+h)/2;
+
+    while(l<=h){
+        if(arr[mid]==key){
+            return arr[mid];
+        }
+        else if(arr[mid]>key){
+            h=mid-1;
+        }
+        else{
+            l=mid+1;
+        }
+        mid= (l+h)/2;
+    }
+    return false;
+}
+
+int main(){
+
+    int size;
+    int key;
+    cout << "Enter size of the array : ";
+    cin >> size;
+    int arr[20];
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    printArray(arr, size);
+    cout << endl;
+
+    cout << "Enter element to be found ";
+    cin >> key;
+
+    int ans = binarySearch(arr,size,key);
+
+    cout<<"Element Present ? "<<ans;
+
+
+return 0;
+}
