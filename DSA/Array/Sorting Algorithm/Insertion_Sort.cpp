0f6c30b21980da84a770eea9e92f6d4be055ed7b
invek <vivekkumar.oct17@gmail.com>
2022-10-02 15:42:47 +0530

    Insertion Sort

--- DSA/Array/Sorting Algorithm/Insertion_Sort.cpp
+++ DSA/Array/Sorting Algorithm/Insertion_Sort.cpp
@@ -12,7 +12,7 @@ void printArray(int arr[], int size)
 
 void selectionSort(int arr[], int size){
     
-    for(int i=0;i<size;i++){
+    for(int i=0;i<size-1;i++){
         int minIndex = i;
 
         for(int j=i+1;j<size;j++){
