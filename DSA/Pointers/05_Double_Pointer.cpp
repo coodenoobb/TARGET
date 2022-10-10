ff02f7e9cc944251c25fb862a9d10addf1859794
invek <vivekkumar.oct17@gmail.com>
2022-10-10 19:51:14 +0530

    Double

---
+++ DSA/Pointers/05_Double_Pointer.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a = 50;
+    int *ptr = &a;
+    int **ptr2 = &ptr;
+
+    cout<<"a : "<<a<<endl;
+    cout<<"ptr : "<<ptr<<endl;
+    cout<<"ptr2 : "<<ptr2<<endl;
+    cout<<"*ptr : "<<*ptr<<endl;
+    cout<<"**ptr2 : "<<**ptr2<<endl;
+    cout<<"*ptr2 : "<<*ptr2<<endl;
+
+return 0;
+}
