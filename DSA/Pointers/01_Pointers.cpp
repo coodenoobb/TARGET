71ce3b1f2ae0364b7eabd49abc9b279f71a36b84
invek <vivekkumar.oct17@gmail.com>
2022-10-10 12:57:14 +0530

    Pointers

--- DSA/Pointers/01_Pointers.cpp
+++ DSA/Pointers/01_Pointers.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 int main(){
     int a = 10;
+    double d = 156.6;
     cout<<"Address of a is "<<&a<<endl;
+
+    int *ptr = &a;
+    double *ptr1 = &d;
+
+    cout<<"Address of a is "<<&a<<endl;
+    cout<<*ptr<<endl;
+
+    cout<<"Address of d is "<<&d<<endl;
+    cout<<*ptr1<<endl;
+
+
 return 0;
 }
