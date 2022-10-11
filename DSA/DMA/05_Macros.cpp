9b748caa9027cf5e36e1514b7b0654cacab9884b
invek <vivekkumar.oct17@gmail.com>
2022-10-11 19:50:55 +0530

    Macros

---
+++ DSA/DMA/05_Macros.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pi 3.14               // Defined PI
+int main(){
+    int r = 5.31;
+
+    double area = pi*r*r;   
+
+    cout<<area<<endl;
+    
+return 0;
+}
