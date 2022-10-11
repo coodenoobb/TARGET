193316ed51aaee1600267df21e534f3bdaf02223
invek <vivekkumar.oct17@gmail.com>
2022-10-11 12:00:46 +0530

    Pass By Value and Refernce

---
+++ DSA/DMA/02_Pass_Refre.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+void update( int &n){
+    n++;
+    
+}
+int main(){
+
+    int a =10;
+    cout<<"Before : "<<a<<endl;
+
+    update(a);
+    cout<<"After : "<<a;
+
+return 0;
+}
