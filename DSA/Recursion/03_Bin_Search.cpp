4af2aa2143e29a093ec8d4924a14be0ce5ea2980
invek <vivekkumar.oct17@gmail.com>
2022-10-18 20:54:54 +0530

    Bin Search using recursion

--- DSA/Recursion/03_Bin_Search.cpp
+++ DSA/Recursion/03_Bin_Search.cpp
@@ -1,28 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int binSearch(int arr[],int target, int l, int h){
+int binSearch(int arr[], int target, int l, int h)
+{
 
-    int mid = l+(h-l)/2;
-
-    while(l<=h){
-        if(arr[mid]==target)
-            return mid;
-
-        else if(arr[mid]>target){
-            return binSearch(arr,target,l,mid-1);
-        }
-        else 
-            return binSearch(arr,target,mid+1,h);
+    if (l > h)
+    {
+        return -1;
     }
-    return -1;
-}
-int main(){
 
-    int arr [6] = {10,20,30,40,50,60};
-    int target = 60;
+    int mid = l + (h - l) / 2;
+
+    if (arr[mid] == target){
+        return mid;
+    }
+
+    else if (arr[mid] > target)
+    {
+        return binSearch(arr, target, l, mid - 1);
+    }
+
+    else
+        return binSearch(arr, target, mid + 1, h);
+}
+int main()
+{
+
+    int arr[6] = {10, 20, 30, 40, 50, 60};
+    int target = 40;
 
     int ans = binSearch(arr, target, 0, 5);
-    cout<<ans<<endl;
+    cout << ans << endl;
 
-return 0;
+    return 0;
 }
