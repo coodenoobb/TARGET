d1fbea8b253bbdaed077867ea00ecfd919bf3c5f
invek <vivekkumar.oct17@gmail.com>
2022-11-12 11:58:54 +0530

    DAMNNNNNNNNNNNNNNNNNNNNN

--- test.cpp
+++ test.cpp
@@ -1,60 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Node{
-    public:
-    int data;
-    Node * next;
-
-    Node(int d){
-        this->data = d;
-        this->next = NULL;
+void printPattern(int row, int col){
+    
+    if(row==0){
+        return ; 
     }
-};
-
-void ReverseList(Node*&head){
-    Node* curr = head;
-    Node* prev = NULL;
-    Node* fwd = NULL;
-
-    while(curr!=NULL){
-        fwd = curr->next;
-        curr->next=prev;
-        prev = curr;
-        curr = fwd;
+    if(row>col){
+        cout<<"*"<<" ";
+        printPattern(row, col+1);
     }
-
-    head = prev;
+    else{
+        cout<<endl;
+        printPattern(row-1,0);
+    }
 
 }
-
-void printList(Node *head)
-{
-    Node *temp = head;
-
-    while (temp != NULL)
-    {
-        cout << temp->data << " ";
-        temp = temp->next;
-    }
-}
-
 int main(){
-    Node *node1 = new Node(3);
-    Node *node2 = new Node(5);
-    Node *node3 = new Node(7);
-    Node *node4 = new Node(9);
-    Node *node5 = new Node(11);
-
-    node1->next = node2;
-    node2->next = node3;
-    node3->next = node4;
-    node4->next = node5;
-
-    printList(node1);
-
-    cout<<endl;
-
-    ReverseList(node1);
-    printList(node1);
+    int num = 4;
+    printPattern(num,0);
+    cout<<"DAMN LUCK";
 return 0;
 }
