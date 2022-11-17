e1438c4ba87a8795e7e4239e822116baed9e54ca
invek <vivekkumar.oct17@gmail.com>
2022-11-17 20:13:55 +0530

    Get Middle Node

--- DSA/Linked List/09_Middle_LinkedList.cpp
+++ DSA/Linked List/09_Middle_LinkedList.cpp
@@ -38,11 +38,29 @@ Node *getMiddle(Node *&head)
     return slow;
 }
 
+Node* getMiddleNode(Node* &head){
+    if(head==NULL){
+        return NULL;
+    }
+
+    Node* slow = head;
+    Node* fast = head;
+
+    while(fast!=NULL && fast->next!=NULL){
+        
+            fast=fast->next->next;
+            slow = slow->next;
+        
+    }
+    return slow;
+
+}
+
 void printList(Node *head)
 {
     Node *temp = head;
 
-    while (temp != NULL)
+    while (temp != NULL )
     {
         cout << temp->data << " ";
         temp = temp->next;
@@ -56,16 +74,24 @@ int main()
     Node *node3 = new Node(7);
     Node *node4 = new Node(9);
     Node *node5 = new Node(11);
+    Node *node6 = new Node(13);
+    Node *node7 = new Node(15);
+    Node *node8 = new Node(17);
+    Node *node9 = new Node(19);
 
     node1->next = node2;
     node2->next = node3;
     node3->next = node4;
     node4->next = node5;
+    node5->next = node6;
+    node6->next = node7;
+    node7->next = node8;
+    node8->next = node9;
 
     printList(node1);
     cout << endl;
 
-    Node *ans = getMiddle(node1);
+    Node *ans = getMiddleNode(node1);
     printList(ans);
 
     return 0;
