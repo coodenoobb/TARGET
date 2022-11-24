981b9833681574e913a37d6d18d0837e701d399e
invek <vivekkumar.oct17@gmail.com>
2022-11-24 19:08:45 +0530

    Looped or Not

--- test.cpp
+++ test.cpp
@@ -1,63 +1,81 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Node{
-    public:
-        int data;
-        Node *next;
+class Node
+{
+public:
+    int data;
+    Node *next;
 
-        Node(int d){
-            this->data = d;
-            this->next = NULL;
-        }
+    Node(int d)
+    {
+        this->data = d;
+        this->next = NULL;
+    }
 };
 
-void helper(Node* &head, Node* cur, Node* pre){
-    //base case
+void printList(Node *head)
+{
+    Node *temp = head;
 
-    if(cur==NULL){
-        head = pre;
-        return;
-    }
-
-    Node * fwd = cur->next;
-    helper(head,fwd,cur);
-    cur->next = pre;
-
-
-}
-Node* reverseList(Node* &head){
-    Node* curr = head;
-    Node* prev = NULL;
-
-    helper(head,curr,prev);
-
-    return head;
-}
-
-void printList(Node* head){
-    Node *temp= head;
-
-    while(temp!=NULL){
-        cout<<temp->data<<" ";
+    while (temp != NULL)
+    {
+        cout << temp->data <<" ";
         temp = temp->next;
     }
 }
-int main(){
 
-    Node *node1 = new Node(3);
-    Node *node2 = new Node(5);
-    Node *node3 = new Node(7);
+bool ifLooped(Node* head){
+    if(head==NULL)  return false;
+
+    map<Node*, bool> visited;
+    Node* temp = head;
+
+    while(temp!=NULL){
+        if(visited[temp]==true) return true;
+
+        visited[temp]=true;
+        temp=temp->next;
+    }
+
+    return false;
+}
+
+int main()
+{
+
+    Node *node1 = new Node(1);
+    Node *node2 = new Node(2);
+    Node *node3 = new Node(3);
+    Node *node4 = new Node(4);
+    Node *node5 = new Node(5);
+    Node *node6 = new Node(6);
+    Node *node7 = new Node(7);
+    Node *node8 = new Node(8);
+    Node *node9 = new Node(9);
+    Node *node10 = new Node(10);
 
     node1->next = node2;
     node2->next = node3;
-   
-    printList(node1);
+    node3->next = node4;
+    node4->next = node5;
+    node5->next = node6;
+    node6->next = node7;
+    node7->next = node8;
+    node8->next = node9;
+    node9->next = node10;
+    node10->next = node5;
+
+    // printList(node1);
     cout<<endl;
-    reverseList(node1);
-    cout<<endl;
-    printList(node1);
 
+    bool isLooped = ifLooped(node1);
 
+    if(isLooped){
+        cout<<"Looped"<<endl;
+    }
+    else{
+        cout<<"Not Looped"<<endl;
+    }
 
-return 0;
+    return 0;
 }
