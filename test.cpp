e5bc5f38707385238d0d8fbd365b281caaf8d11f
invek <vivekkumar.oct17@gmail.com>
2022-11-27 18:09:41 +0530

    *

--- test.cpp
+++ test.cpp
@@ -12,6 +12,58 @@ public:
         this->next = NULL;
     }
 };
+void insertAtTail(Node* &tail, Node* curr){
+    tail->next = curr;
+    tail = curr;
+}
+
+Node* sortList(Node *head)
+{
+    // Write your code here.
+    Node* zeroHead = new Node(-99);
+    Node* zeroTail = zeroHead;
+    Node* oneHead = new Node(-99);
+    Node* oneTail = oneHead;
+    Node* twoHead = new Node(-99);
+    Node* twoTail = twoHead;
+    
+    Node* curr = head;
+    
+    // creating three lists for 0s , 1s and 2s
+    while(curr!=NULL){
+        int val = curr->data;
+        
+        if(val==0){
+            insertAtTail(zeroTail,curr);
+        }
+        else if(val == 1){
+            insertAtTail(oneTail,curr);
+        }    
+        else if(val == 2){
+            insertAtTail(twoTail,curr);
+        }    
+        curr = curr->next;      
+    }
+    
+    // merge all three lists
+    if(oneHead->next!=NULL){
+        zeroTail->next = oneHead->next;
+    }
+    else{
+        zeroTail->next = twoHead->next;
+    }
+    
+    oneTail->next = twoHead->next;
+    twoTail->next = NULL;
+
+    head = zeroHead->next;
+
+    delete(oneHead);
+    delete(twoHead);
+    delete(zeroHead);
+    return head;
+ 
+}
 
 void printList(Node *head)
 {
@@ -24,88 +76,33 @@ void printList(Node *head)
     }
 }
 
-Node* floydCycleDetection(Node* head){
-    if(head==NULL)  return NULL;
 
-    Node* slow = head;
-    Node* fast = head;
-
-    while(fast!=NULL and slow!=NULL){
-        fast = fast->next;
-        if(fast!=NULL)  fast=fast->next;
-
-        slow=slow->next;
-        if(slow==fast)  return slow;
-        
-    }
-
-    return NULL;
-}
-
-Node* getStartingNode(Node* head){
-    if(head==NULL)  return NULL;
-
-    Node* intersection = floydCycleDetection(head);
-
-    Node* slow = head;
-    Node* fast = intersection;
-
-    while(slow!=fast){
-        slow=slow->next;
-        fast=fast->next;
-    }
-
-    return slow;
-
-}
-
-Node* removeLoop(Node* head){
-    if(head==NULL)  return NULL;
-
-
-    Node* startNode = getStartingNode(head);
-    Node* temp = startNode;
-
-    while(temp->next!=startNode){
-        temp = temp->next;
-    }
-
-    temp->next = NULL;
-    return head;
-
-}
 
 int main()
 {
 
     Node *node1 = new Node(1);
-    Node *node2 = new Node(2);
-    Node *node3 = new Node(3);
-    Node *node4 = new Node(4);
-    Node *node5 = new Node(5);
-    Node *node6 = new Node(6);
-    Node *node7 = new Node(7);
-   
+    Node *node2 = new Node(1);
+    Node *node3 = new Node(2);
+    Node *node4 = new Node(0);
+    Node *node5 = new Node(1);
+    Node *node6 = new Node(2);
+    
 
     node1->next = node2;
     node2->next = node3;
     node3->next = node4;
     node4->next = node5;
     node5->next = node6;
-    node6->next = node7;
-    node7->next = node5;
-    
-    
 
-    Node* flyod = floydCycleDetection(node1);
-    cout<<"Intersection of Slow and Fast is at "<<flyod->data<<endl;
 
-    Node* startOfLoop = getStartingNode(node1);
-    cout<<"Start of Loop is at "<<startOfLoop->data<<endl;
+    printList(node1);
+    cout<<endl;
+    Node* ans = sortList(node1);
 
-    Node* withoutLoop = removeLoop(node1);
-    cout<<"List without loop"<<endl;
-    printList(withoutLoop);
+    printList(ans);
+
+ 
 
    
     return 0;
