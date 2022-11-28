47856e28e7440b120aa17a74f760e6236ab184ef
invek <vivekkumar.oct17@gmail.com>
2022-11-28 14:38:15 +0530

    Rverse in K-grp

--- DSA/Linked List/10_Reverse_LL_in_K_grp.cpp
+++ DSA/Linked List/10_Reverse_LL_in_K_grp.cpp
@@ -20,23 +20,34 @@ void printList(Node* head){
 }
 
 Node* reverseKlist(Node* &head,int k){
-
     if(head==NULL){
         return head;
     }
-
-    // Reverse nodes
-
+    
+    // step-1 : Reverse k nodes itera
+    
     Node* curr = head;
     Node* prev = NULL;
     Node* fwd = NULL;
-
-    while(k!=0){
+    int count = 0;
+    
+    while(count<k && curr!=NULL){
         fwd = curr->next;
         curr->next = prev;
         prev = curr;
         curr = fwd;
+        count++;
     }
+    
+    // step-2 Recursion
+    
+    if(fwd!=NULL){
+        head->next = reverseKlist(fwd,  k);
+    }
+    
+    //step-3 return head of reversed node;
+    
+    return prev;
 
 }
 
@@ -47,9 +58,18 @@ int main(){
     Node *node2 = new Node(5);
     Node *node3 = new Node(7);
     Node *node4 = new Node(9);
+    Node *node5 = new Node(11);
+    Node *node6 = new Node(13);
 
     node1->next = node2;
     node2->next = node3;
     node3->next = node4;
+    node4->next = node5;
+    node5->next = node6;
+
+    printList(node1);
+    cout<<endl;
+    Node* ans = reverseKlist(node1,2);
+    printList(ans);
 return 0;
 }
