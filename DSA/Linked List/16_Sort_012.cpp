ab7aeb205c4306afd97bda5a54fa30cfda7e61fd
invek <vivekkumar.oct17@gmail.com>
2022-11-28 16:18:13 +0530

    *

--- DSA/Linked List/16_Sort_012.cpp
+++ DSA/Linked List/16_Sort_012.cpp
@@ -67,7 +67,6 @@ Node* sortList(Node *head)
     Node* oneTail = oneHead;
     Node* twoHead = new Node(-99);
     Node* twoTail = twoHead;
-    
     Node* curr = head;
     
     // creating three lists for 0s , 1s and 2s
@@ -84,8 +83,7 @@ Node* sortList(Node *head)
             insertAtTail(twoTail,curr);
         }    
         curr = curr->next;      
-    }
-    
+    }   
     // merge all three lists
     if(oneHead->next!=NULL){
         zeroTail->next = oneHead->next;
@@ -96,9 +94,8 @@ Node* sortList(Node *head)
     
     oneTail->next = twoHead->next;
     twoTail->next = NULL;
-
     head = zeroHead->next;
-
+    
     delete(oneHead);
     delete(twoHead);
     delete(zeroHead);
@@ -121,6 +118,8 @@ int main(){
     Node *node4 = new Node(0);
     Node *node5 = new Node(1);
     Node *node6 = new Node(2);
+    Node *node7 = new Node(0);
+    Node *node8 = new Node(0);
     
 
     node1->next = node2;
@@ -128,6 +127,8 @@ int main(){
     node3->next = node4;
     node4->next = node5;
     node5->next = node6;
+    node6->next = node7;
+    node7->next = node8;
 
     
 
