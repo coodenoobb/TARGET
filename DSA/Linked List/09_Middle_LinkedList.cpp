0050ef4e25ea68db8a4fddc7d0eb2f8f1395f9de
invek <vivekkumar.oct17@gmail.com>
2022-11-29 16:11:35 +0530

    Check Palindrome

--- DSA/Linked List/09_Middle_LinkedList.cpp
+++ DSA/Linked List/09_Middle_LinkedList.cpp
@@ -70,23 +70,17 @@ int main()
 {
 
     Node *node1 = new Node(3);
-    Node *node2 = new Node(5);
-    Node *node3 = new Node(7);
-    Node *node4 = new Node(9);
-    Node *node5 = new Node(11);
-    Node *node6 = new Node(13);
-    Node *node7 = new Node(15);
-    Node *node8 = new Node(17);
-    Node *node9 = new Node(19);
+    Node *node2 = new Node(3);
+    Node *node3 = new Node(1);
+    Node *node4 = new Node(1);
+    Node *node5 = new Node(7);
+    Node *node6 = new Node(7);
 
     node1->next = node2;
     node2->next = node3;
     node3->next = node4;
     node4->next = node5;
     node5->next = node6;
-    node6->next = node7;
-    node7->next = node8;
-    node8->next = node9;
 
     printList(node1);
     cout << endl;
---
+++ DSA/Linked List/18_Check_Palindrome.cpp
@@ -0,0 +1,101 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+        int data;
+        Node *next;
+
+        Node(int d){
+            this->data = d;
+            this->next = NULL;
+        }
+};
+void printList(Node* head){
+    Node *temp= head;
+
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+}
+Node* getMiddleNode(Node* &head){
+
+    Node* slow = head;
+    Node* fast = head->next;
+
+    while(fast!=NULL and fast->next!=NULL){
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+
+    return slow;
+}
+Node* reverseList(Node* head){
+    if(head==NULL or head->next==NULL) return head;
+
+    Node* newHead = reverseList(head->next);
+    head->next->next = head;
+    head->next = NULL;
+
+    return newHead;
+}
+bool checkPalindrome(Node* head){
+
+    // get middle Node then reverse from that node
+    Node* middleNode = getMiddleNode(head);
+    
+    // assign reversed list in the new list 
+    Node* temp = middleNode->next;
+    middleNode->next = reverseList(temp);
+
+    // comapre both part of the list 
+
+    Node* head1 = head;
+    Node* head2 = middleNode->next;     
+
+    while(head2!=NULL){
+        if(head1->data!=head2->data) {   
+            return false;
+        }
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+    return true;
+}
+int main(){
+
+    Node *node1 = new Node(1);
+    Node *node2 = new Node(2);
+    Node *node3 = new Node(3);
+    Node *node4 = new Node(3);
+    Node *node5 = new Node(2);
+    Node *node6 = new Node(1);
+
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+    node4->next = node5;
+    node5->next = node6;
+   
+    printList(node1);
+    cout<<endl;
+
+    // Node* midNode = getMiddleNode(node1);
+    // printList(midNode);
+    // cout<<endl;
+
+    // Node* revNode = reverseList(node1);
+    // printList(revNode);
+    // cout<<endl;
+
+
+   
+    cout<<endl;
+    bool ans = checkPalindrome(node1);
+    cout<<ans;
+    
+
+
+
+return 0;
+}
