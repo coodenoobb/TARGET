67d96d6d4534a29f3b8213474dfc3a82dc0ab42d
invek <vivekkumar.oct17@gmail.com>
2022-12-13 23:44:16 +0530

    *

--- test.cpp
+++ test.cpp
@@ -1,29 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-    bool halvesAreAlike(string s) {
-        int n = s.length();
-        int cnt1,cnt2 = 0;
-        for(int i = 0;i<n/2;i++){
-            if(s[i]=='a'|| s[i]=='e' || s[i]=='i'|| s[i]== 'o'|| s[i]=='u'|| 
-            s[i]=='A'|| s[i]=='E'|| s[i]== 'I'|| s[i]== 'O'|| s[i]== 'U'){
-                cnt1++;
-            }
-            
-        }
-        for(int i=n/2;i<n;i++){
-            if(s[i]=='a'|| s[i]=='e' || s[i]=='i'|| s[i]== 'o'|| s[i]=='u'|| 
-            s[i]=='A'|| s[i]=='E'|| s[i]== 'I'|| s[i]== 'O'|| s[i]== 'U'){
-                cnt2++;
-            }
-        }
-
-        if(cnt1==cnt2)  return true;
-        else   return false;
-    }
-
 int main(){
-    string str = "booook";
-    bool ans = halvesAreAlike(str);
-    cout<<ans;
+    for(auto i=1 ;i<=5;i++){
+  for(auto j=1;j<i;j++){
+    cout<<"*";
+  }
+  cout<<"\n";
+}
 return 0;
 }
