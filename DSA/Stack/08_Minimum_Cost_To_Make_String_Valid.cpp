b517fc8553c87e49a468986c036b9bd89c9c0dc0
invek <vivekkumar.oct17@gmail.com>
2022-12-19 23:28:52 +0530

    Find min cost for a valid bracket

--- DSA/Stack/08_Minimum_Cost_To_Make_String_Valid.cpp
+++ DSA/Stack/08_Minimum_Cost_To_Make_String_Valid.cpp
@@ -1,6 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
+int findMinCost(string &str){
+    if(str.length()%2!=0)   return -1;
+
+    // we remove the valid brackets
+    stack <char> st;
+    for(int i=0;i<str.length();i++){
+        char ch = str[i];
+
+        if(ch=='{'){
+            st.push(ch);
+        }
+        else{
+            if(!st.empty() && st.top()=='{'){
+                st.pop();
+            }
+            else{
+                st.push(ch);
+            }
+        }
+    }
+    // we count no. of brackets
+    int closedBrackets=0,openBrackets = 0; 
+
+    while(!st.empty()){
+        if(st.top()=='{')   closedBrackets++;
+        else    openBrackets++;
+
+        st.pop();
+    }
+    int ans = (closedBrackets+1)/2 + (openBrackets+1)/2;
+
+    return ans;
+}
 int main(){
- 
+
+    string a = "{}{{{{}}";
+    int ans = findMinCost(a);
+    cout<<ans;
+
 return 0;
 }
