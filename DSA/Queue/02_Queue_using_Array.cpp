ab374c7ee8101c9afe7d1b0cebf9c54eeb5f35a1
invek <vivekkumar.oct17@gmail.com>
2022-12-28 17:30:08 +0530

    Queue using Array

--- DSA/Queue/02_Queue_using_Array.cpp
+++ DSA/Queue/02_Queue_using_Array.cpp
@@ -22,20 +22,15 @@ class Queue{
         }
     }
 
-    int pop(){
+    void pop(){
         if(front==rear){
-            return -1;
+            cout<<"Queue is Empty";
         }
         else{
-            int ans = arr[front];
-            arr[front]=-1;
-            front++;
-
-            if(front==rear){
-                front=0;
-                rear=0;
+            for(int i=0;i<rear-1;i++){
+                arr[i] = arr[i+1];
             }
-            return ans;
+            rear--;
         }
     }
     
@@ -58,19 +53,26 @@ class Queue{
     }
 };
 
+void printQueue(Queue Q){
+    while(!Q.isEmpty()){
+		cout<<" "<<Q.qfront();
+		Q.pop();
+	}
+}
 
 int main(){
     Queue q1(4);
 
-    q1.push(10); 
+    q1.push(10);
+    q1.push(11);
+    q1.push(11);
+    q1.push(13);
+    printQueue(q1);
+    cout<<endl;
     q1.pop();
-    q1.push(20); 
-    q1.push(30); 
-    q1.push(40); 
-    
 
-    cout<<q1.isEmpty()<<endl;
-
-    cout<<q1.qfront();
+    q1.push(20);
+    q1.push(21);
+    printQueue(q1);
 return 0;
 }
